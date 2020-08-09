8975fdc49707a0fba02c93be319cc0659ef40c23
Sushmita <susmita.mca@gmail.com>
2020-08-09 17:04:43 +0530

    Initial Commit

---
+++ LodgeBookingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LodgeBookingSystem.Startup))]
+namespace LodgeBookingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
